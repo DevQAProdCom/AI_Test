bf95134ec256971d239f5d6f034b0d8d84a519e0
Vladyslav Babarkin <vladyslav_babarkin@epam.com>
2026-06-10 16:00:02 +0300

    new project

---
+++ Tests.AI_TEST.Playwright/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Tests.AI_TEST.Playwright
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
